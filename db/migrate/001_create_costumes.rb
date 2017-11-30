48c15e9fe39ef82484247cf81c35f1fea4503842
Amanda Ackerman <ackerm44@gmail.com>
2017-11-30 19:15:52 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,12 @@
-# Create a class and inherit from ActiveRecord::Migration
 class CreateCostumes << ActiveRecord::Migration
   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.price :float
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
   end
 end
 
